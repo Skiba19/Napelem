46db2265809e007bc4480ca4f1d375fe0034b3a5
Klein Gergő <klein.gergo2000@gmail.com>
2023-05-08 13:51:16 +0200

    Done

---
+++ Napelem-API/Napelem-API/Migrations/20230505231206_update.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Napelem_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "objectType",
+                table: "Employees");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "objectType",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
