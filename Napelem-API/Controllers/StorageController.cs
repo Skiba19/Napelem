31636ed70d769b5d382f35b6bcdebe4095b2cd01
Skiba19 <skibagergo@gmail.com>
2023-05-08 18:08:01 +0200

    Storage

--- Napelem-API/Controllers/StorageController.cs
+++ Napelem-API/Controllers/StorageController.cs
@@ -5,21 +5,33 @@ using Napelem_API.Models;
 
 namespace Napelem_API.Controllers
 {
+    public class ComponentStorage
+    {
+        public Component Component { get; set; }
+        public Storage Storage { get; set; }
+    }
     [Route("api/[controller]")]
     [ApiController]
     public class StorageController : ControllerBase
     {
         //Add Component
-        /*[HttpPost("AddComponentToStorage")]
-        public JsonResult AddComponentToStorage(Component component, Storage storage)
+        [HttpPost("AddComponentToStorage")]
+        public JsonResult AddComponentToStorage(ComponentStorage componentStorage)
         {
             using (NapelemContext context = new NapelemContext())
             {
-                storage.componentID=component.componentID;
-                context.Components.Add(component);
+                Storage storage = new Storage
+                {
+                    componentID = componentStorage.Component.componentID,
+                    row=componentStorage.Storage.row,
+                    column=componentStorage.Storage.column,
+                    level = componentStorage.Storage.level
+                };
+                
+                context.Storages.Add(storage);
                 context.SaveChanges();
             }
-            return new JsonResult(Ok(component));
-        }*/
+            return new JsonResult(Ok(componentStorage));
+        }
     }
 }
